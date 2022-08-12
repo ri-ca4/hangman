466c6ab095c67e1109db6586a34e4ab8f7dc91ee
ri-ca4 <riley.de12@gmail.com>
2022-08-12 19:04:53 -0400

    work on check function and strike system

--- js/hangman.js
+++ js/hangman.js
@@ -6,34 +6,61 @@
 
 //TODO: this needs to be a function to be called on button push
 var wordDisp = document.getElementById('word');
-//get word
 var playerWord = prompt('player 1 enter word');
-//TODO: validate that it is a single word with no symbols or spaces, and to uppercase 
-//create "blanks"
-for(i=0; i<playerWord.length; i++){
+//TODO: validate that it is a single word with no symbols or spaces, and to uppercase
+
+
+for(i=0; i<playerWord.length; i++){ //create "blanks"
     wordDisp.innerHTML += '<div class="letter" id="' + playerWord[i] + '">-</input>';
 }
 
+var letters = playerWord.split('');//create letters array
+console.log(letters)
+
+
 var btn = document.getElementById('enter');
-btn.addEventListener('click', () =>{
+btn.addEventListener('click', () =>{//validate that a single letter has been provided
     var guess = document.getElementById('guess').value
     var regex = new RegExp("[a-zA-Z]");
     if(regex.test(guess)){
-        alert("true");
-        check();
+        //alert("true");
+        check(guess);
         }else{
-            alert("please input 1 letter");
+            alert("please input 1 letter");            
     }
 })
 
-function check(){
-    alert("check")
+var guesses = [];
+var strikes = 0;
+
+function check(guess){//check input
+    //alert(guess);
+    if(guesses.includes(guess)){//has it been guessed already?
+        alert(guess + " has already been guessed");
+    }else{//add letter to guesses array, check if letter is in word
+        guesses.push(guess);
+        console.log(guesses)
+
+        if(letters.includes(guess)){//is the letter part of the word?
+            console.log(guess + " is part of " + playerWord)
+            document.getElementById(guess).innerHTML = guess;//display letter
+            
+        }else{
+            console.log(guess + " is not part of " + playerWord)
+            strikes = strikes + 1;
+            console.log(strikes);
+            strike()
+        }
+    }
 }
-//check input
-//has the letter been used?
-//is the letter part of the word?
-//display if y, add strike if no
 
-//strike out
+function strike(){
+    alert('you have ' + strikes + ' strikes')
+    //strike out
+}
 
-//win
+
+function win(){
+    alert("winner!")
+    //win
+}
